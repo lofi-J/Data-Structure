4f83af2eea0098eee806f74a0d8909a6529a4d45
lofi-J <jsj2505@gmail.com>
2022-05-06 00:00:19 +0900

    add Dynamic_Array

---
+++ Dynamic_Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Dynamic_Array
+{
+    private object[] arr = new object[0];
+    public void Add(object element)
+    {
+        var temp = new object[arr.Length + 1];
+
+        for ( int i = 0; i < arr.Length; i++) //arr[] -> temp[] copy
+        {
+            temp[i] = arr[i];
+        }
+        arr = temp;
+
+        arr[arr.Length - 1] = element; //add element
+    }
+
+    public void Peek()
+    {
+        foreach(object x in arr)
+        {
+            Console.Write(x + ", ");
+        }
+        Console.WriteLine("\b\b ");
+    }
+}
+/*
+Dynamic_Array myDynamicArr = new Dynamic_Array();
+        myDynamicArr.Add(0);
+        myDynamicArr.Add(1);
+        myDynamicArr.Add(2);
+        myDynamicArr.Peek();
+
+
+result
+
+0, 1, 2
+
+ */
