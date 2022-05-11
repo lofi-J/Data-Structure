01f355c9f5efe72ab4f23704b1be9ae992833dbc
lofi-J <jsj2505@gmail.com>
2022-05-11 18:53:57 +0900

    add Stack_Using_Array

---
+++ Stack_Using_Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class StackUsingArray
+{
+    object[] stack = new object[20];
+    private int top = -1;
+
+    public void Push(object data)
+    {
+        if (top == stack.Length - 1)
+            throw new InvalidOperationException();
+            //ResizeStack(); //throw or Resizing
+                
+        stack[++top] = data;
+    }
+
+    public object Pop()
+    {
+        if (this.IsEmpty)
+            throw new ApplicationException("Empty");
+
+        return stack[top--];
+    }
+
+    public object Peek()
+    {
+        if (this.IsEmpty)
+            throw new ApplicationException("Empty");
+        else
+            return stack[top];
+    }
+
+    public bool IsEmpty
+    {
+        get { return top == -1; }
+    }
+
+}
