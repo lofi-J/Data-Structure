89b7db9bfbf3e8a3d262bf80c4ba5a7ccd2ad9b8
lofi-J <jsj2505@gmail.com>
2022-05-12 23:19:18 +0900

    add Stack_Using_Linked_List

---
+++ Stack_Using_Linked_List.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class StackNode
+{
+    public object data { get; set; }
+    public StackNode next;
+
+    public StackNode(object data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class StackUsingLinkedList
+{    
+    private StackNode top = null;
+
+    public bool IsEmpty
+    {
+        get { return top == null; }
+    }
+
+    public void Push(object data)
+    {
+        if (top == null)
+        {
+            top = new StackNode(data);
+        }
+        else
+        {
+            var node = new StackNode(data);
+            node.next = top;
+            top = node;            
+        }
+    }
+
+    public object Pop()
+    {
+        if (this.IsEmpty)
+            throw new ApplicationException("Empty");
+        else
+        {
+            object data = top.data;
+            top = top.next;
+            return data;
+        }
+    }
+
+    public object Peek()
+    {
+        if (this.IsEmpty)
+            throw new ApplicationException("Empty");
+
+        return top.data;
+    }
+}
